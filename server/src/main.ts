6feb67b6699a347aa9a0517071e3302b1feae7ac
Om-Khode <omkhode2003@gmail.com>
2024-03-27 18:42:15 +0530

    Remove commented out code

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,8 +5,6 @@ import { BlockchainServer } from './blockchain-server';
 
 const PORT = 3000;
 const app = express();
-// app.use('/',             express.static(path.join(__dirname, '../client/dist')));
-// app.use('/node_modules', express.static(path.join(__dirname, '../../node_modules')));
 
 const httpServer: http.Server = app.listen(PORT, () => {
   if (process.env.NODE_ENV !== 'production') {
